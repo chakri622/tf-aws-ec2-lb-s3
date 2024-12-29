a4b51caf1dbb946afb718b8f39698a9d4e0754da
SPACEVERSE\chakr <chakradhar.botcha@gmail.com>
2024-12-28 23:52:29 -0600

    Fixed format issue

--- instances.tf
+++ instances.tf
@@ -9,10 +9,10 @@ data "aws_ssm_parameter" "amzn2_linux" {
 
 #instances
 resource "aws_instance" "nginx_instances" {
-  count                  = var.instances_count
-  ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type          = var.instance_type
-  
+  count         = var.instances_count
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type = var.instance_type
+
   subnet_id              = module.app.public_subnets[count.index % var.vpc_public_subnet_count]
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
   tags                   = local.common_tags
